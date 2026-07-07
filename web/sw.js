3f84bd148266a738fe0ba39627c32245bd5c3a72
Yan Myo Aaron <ymyo44277@gmail.com>
2026-07-07 15:02:12 +0630

    ui: compact GPS toggle, smaller inputs, inline toggle

--- web/sw.js
+++ web/sw.js
@@ -1,17 +1,3 @@
-const CACHE = "antifake-v1";
-const URLS = ["/", "/manifest.json"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(URLS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => r))
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
+self.addEventListener("fetch", (e) => e.respondWith(fetch(e.request)));
